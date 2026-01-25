0eff55e7e5d57ba03cdefea01589bf1b22e87960
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-01-25 22:01:01 +0300

    Setting up backend for hosting

--- front-end-design/short-it/src/main.tsx
+++ front-end-design/short-it/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import "./index.css";
 import App from "./App.tsx";
 import { system } from "./theme.ts";
