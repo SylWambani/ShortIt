2ef8198fd4541ff0e588884e3d02fc293ce1c164
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-01-29 07:14:05 +0300

    Error fix, connect front end and backend

--- front-end-design/short-it/src/components/api/axiosInstance.ts
+++ front-end-design/short-it/src/components/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:'https://shortit-6b2j.onrender.com ',
+  baseURL:import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
--- front-end-design/short-it/src/components/api/publicAxiosInstance.tsx
+++ front-end-design/short-it/src/components/api/publicAxiosInstance.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const publicAxios = axios.create({
-  baseURL: "https://shortit-6b2j.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
